b9cd0f7137a4d93188b25d660b2c4321e25ff48e
Tirtho-Ray <tirthoray10@gmail.com>
2026-02-25 10:04:19 +0600

    chage the data formate

--- src/modules/submitted/submitted.module.ts
+++ src/modules/submitted/submitted.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { SubmittedController } from './controller/submitted.controller';
 import { SubmittedService } from './service/submitted.service';
 import { NotificationModule } from '../notification/notification.module';
+import { ChartMainService } from '../chart/service/chart.main.service';
 
 @Module({
    imports: [NotificationModule], 
   controllers: [SubmittedController],
-  providers: [SubmittedService],
+  providers: [SubmittedService,  ChartMainService ],
   exports: [SubmittedService],
 })
 export class SubmittedModule {}
